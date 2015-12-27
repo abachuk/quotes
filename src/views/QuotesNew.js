a1534502c727efbe4dcfed191ca2f26a7f168f9b
Alex Bachuk <alex@bachuk.com>
2015-12-27 13:44:57 -0500

    chore (connecting all inputs, clean up)

--- src/views/QuotesNew.js
+++ src/views/QuotesNew.js
@@ -2,16 +2,11 @@ import React from 'react'
 import 'styles/core.scss'
 import Firebase from 'firebase'
 import constants from 'utils/constants'
-import { connect } from 'react-redux'
 import {reduxForm} from 'redux-form'
 
 const ref = new Firebase(constants.FIREBASE)
 const quotesRef = ref.child('quotes')
 export const fields = ['title', 'description', 'author', 'tags', 'category']
-
-const mapStateToProps = (state) => ({
-  auth: state.auth
-})
 
 export class QuotesNew extends React.Component {
 
@@ -69,22 +64,22 @@ export class QuotesNew extends React.Component {
 
           <div className='form-group'>
             <label forHtml='description'>Description</label>
-            <textarea className='form-control' rows='4' id='description' ref='description' placeholder='text or description' />
+            <textarea className='form-control' rows='4' id='description' ref='description' placeholder='text or description' {...description} />
           </div>
 
           <div className='form-group'>
             <label forHtml='author'>Author</label>
-            <input type='text' className='form-control' id='author' placeholder='author' ref='author' />
+            <input type='text' className='form-control' id='author' placeholder='author' ref='author' {...author} />
           </div>
 
           <div className='form-group'>
             <label forHtml='category'>Category</label>
-            <input type='text' className='form-control' id='category' placeholder='category (quote, joke, etc)' ref='category' />
+            <input type='text' className='form-control' id='category' placeholder='category (quote, joke, etc)' ref='category' {...category} />
           </div>
 
           <div className='form-group'>
             <label forHtml='tags'>Tags</label>
-            <input type='text' className='form-control' id='tags' placeholder='tags (comma separated)' ref='tags' />
+            <input type='text' className='form-control' id='tags' placeholder='tags (comma separated)' ref='tags' {...tags} />
           </div>
 
           <input type='submit' value='Create quote' className='btn btn-primary' />

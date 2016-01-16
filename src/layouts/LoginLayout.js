7469206ab93de6c0532e22ae80144fd5251b0b2f
Alex Bachuk <alex@bachuk.com>
2016-01-15 20:24:50 -0500

    chore (comments and bump version of redux-form)

--- src/layouts/LoginLayout.js
+++ src/layouts/LoginLayout.js
@@ -1,3 +1,9 @@
+/**
+  This is a login layout which manages login and register views.
+  Here we check if user signed in already then redirect to profile.
+  We pass history to children components
+**/
+
 import React from 'react'
 import styles from '../styles/LoginStyles.scss'
 import { connect } from 'react-redux'
@@ -19,7 +25,6 @@ export class LoginLayout extends React.Component {
     if (this.props.auth) {
       this.props.history.pushState(null, '/profile')
     }
-    console.log(this)
   }
 
   render () {

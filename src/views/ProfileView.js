80accf56e99e70face7420a85eca77d97db8d3f6
Alex Bachuk <alex@bachuk.com>
2016-01-27 08:22:53 -0500

    feat (profile form)

--- src/views/ProfileView.js
+++ src/views/ProfileView.js
@@ -4,7 +4,45 @@ import 'styles/core.scss'
 export class ProfileView extends React.Component {
   render () {
     return (
+      <div className='container'>
       <h1>My profile</h1>
+
+        <form onSubmit={this.handleSubmit.bind(this)}>
+
+          <div className='form-group'>
+            <label forHtml='title'>Name</label>
+            <input type='text' className='form-control' id='title' placeholder='Name' ref='title' {...title} />
+          </div>
+
+          <div className='form-group'>
+            <label forHtml='description'>Description</label>
+            <textarea className='form-control' rows='4' id='description' ref='description' placeholder='text or descriptions' {...description} />
+          </div>
+
+          <div className='form-group'>
+            <label forHtml='author'>Author</label>
+            <input type='text' className='form-control' id='author' placeholder='author' ref='author' {...author} />
+          </div>
+
+          <div className='form-group'>
+            <label forHtml='category'>Category</label>
+            <input type='text' className='form-control' id='category' placeholder='category (quote, joke, etc)' ref='category' {...category} />
+          </div>
+
+          <div className='form-group'>
+            <label forHtml='tags'>Tags</label>
+            <input type='text' className='form-control' id='tags' placeholder='tags (comma separated)' ref='tags' {...tags} />
+          </div>
+
+          <div className='form-group'>
+            <label forHtml='image'>Background</label>
+            <input type='file' className='form-control' id='image' placeholder='background image' ref='image' {...image} value={null} />
+          </div>
+
+          <input type='submit' value={btnLabel} className='btn btn-primary' />
+          <input type='button' value='Reset' className='btn btn-danger' onClick={resetForm} />
+        </form>
+      </div>
     )
   }
 }
